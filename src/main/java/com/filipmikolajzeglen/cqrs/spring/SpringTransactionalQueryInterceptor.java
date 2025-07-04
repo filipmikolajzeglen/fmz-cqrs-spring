d663b475894286e1e2143050aad267b01d682df8
Filip Żegleń <filipmikolajzeglen@gmail.com>
2025-07-04 23:11:07 +0200

    FMZ-CQRS-015 Update versions of fmz-cqrs-core and fmz-cqrs-persistence in pom.xml

--- src/main/java/com/filipmikolajzeglen/cqrs/spring/SpringTransactionalQueryInterceptor.java
+++ src/main/java/com/filipmikolajzeglen/cqrs/spring/SpringTransactionalQueryInterceptor.java
@@ -1,8 +1,8 @@
 package com.filipmikolajzeglen.cqrs.spring;
 
-import com.filipmikolajzeglen.cqrs.core.Pagination;
 import com.filipmikolajzeglen.cqrs.core.Query;
 import com.filipmikolajzeglen.cqrs.core.QueryInvocation;
+import com.filipmikolajzeglen.cqrs.core.ResultStrategy;
 import com.filipmikolajzeglen.cqrs.core.TransactionalQueryInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,9 +16,9 @@ public class SpringTransactionalQueryInterceptor extends TransactionalQueryInter
 {
    @Override
    @Transactional(readOnly = true)
-   public <TYPE, PAGE> PAGE intercept(
-         Query<TYPE> query, Pagination<TYPE, PAGE> pagination, QueryInvocation<TYPE, PAGE> next)
+   public <TYPE, RESULT> RESULT intercept(
+         Query<TYPE> query, ResultStrategy<TYPE, RESULT> resultStrategy, QueryInvocation<TYPE, RESULT> next)
    {
-      return super.intercept(query, pagination, next);
+      return super.intercept(query, resultStrategy, next);
    }
 }
--- src/test/java/com/filipmikolajzeglen/cqrs/spring/SampleQueryHandler.java
+++ src/test/java/com/filipmikolajzeglen/cqrs/spring/SampleQueryHandler.java
@@ -1,15 +1,15 @@
 package com.filipmikolajzeglen.cqrs.spring;
 
-import com.filipmikolajzeglen.cqrs.core.Pagination;
 import com.filipmikolajzeglen.cqrs.core.QueryHandler;
+import com.filipmikolajzeglen.cqrs.core.ResultStrategy;
 import org.springframework.stereotype.Service;
 
 @Service
 class SampleQueryHandler implements QueryHandler<SampleQuery, String>
 {
    @Override
-   public <PAGE> PAGE handle(SampleQuery query, Pagination<String, PAGE> pagination)
+   public <RESULT> RESULT handle(SampleQuery query, ResultStrategy<String, RESULT> resultStrategy)
    {
-      return pagination.expandSingle("Result for: " + query.input);
+      return resultStrategy.expandSingle("Result for: " + query.input);
    }
 }
